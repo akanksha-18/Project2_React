e04918f3eead0ab223d613b6e46f50b2999bfe3d
Akanksha Dubey <akankshadubey2558@gmail.com>
2024-05-03 18:57:43 +0530

    ommiting

--- LoremIpsum/src/components/output.jsx
+++ LoremIpsum/src/components/output.jsx
@@ -1,17 +1,59 @@
+// import React from "react";
+
+// const Output = (props) => {
+//     const { paragraphs,  includeHTML ,tag} = props;
+
+//     return (
+//         <div className="output">
+//             {includeHTML === "Yes" ? (
+//                 <p>{paragraphs.map((p)=>`<${tag}>`+paragraphs+`<${tag}/>`)}</p>
+//             ):(
+//                <p>{paragraphs.map((p)=>paragraphs)}</p>
+//             )}
+//         </div>
+//     );
+// }
+
+// export default Output;
+
+// import React from "react";
+
+// const Output = (props) => {
+//     const { paragraphs, includeHtml, tag } = props;
+
+//     return (
+//         <div className="output">
+//             {includeHtml === "Yes" ? (
+//                 paragraphs.map((p, index) => (
+//                     <p key={index} dangerouslySetInnerHTML={{ __html: `<${tag}>${p}</${tag}>` }} />
+//                 ))
+//             ) : (
+//                 paragraphs.map((p, index) => <p key={index}>{p}</p>)
+//             )}
+//         </div>
+//     );
+// }
+
+// export default Output;
+
 import React from "react";
 
 const Output = (props) => {
-    const { paragraphs,  includeHTML ,tag} = props;
+    const { paragraphs, includeHtml, tag } = props;
 
     return (
         <div className="output">
-            {includeHTML === "Yes" ? (
-                <p>{paragraphs.map((p)=>`<${tag}>`+paragraphs+`<${tag}/>`)}</p>
-            ):(
-               <p>{paragraphs.map((p)=>paragraphs)}</p>
+            {includeHtml === "Yes" ? (
+                paragraphs.map((p, index) => (
+                    <p key={index}>{`<${tag}>${p}</${tag}>`}</p>
+                ))
+            ) : (
+                paragraphs.map((p, index) => <p key={index}>{p}</p>)
             )}
         </div>
     );
 }
 
 export default Output;
+
+
